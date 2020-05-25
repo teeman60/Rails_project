972727a76a88835d4dc48830f02328c587716e06
teeman60 <taiye41@gmail.com>
2020-05-25 18:02:19 -0500

    updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,14 @@ class SessionsController < ApplicationController
         # byebug
     user = User.find_by(user_name: params[:user_name])
         if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
             redirect_to user
         else
             redirect_to '/login'
         end
     # user = user.try(:authenticate, params[:user][:password])
     # return redirect_to(controller: 'sessions', action: 'new') unless user
-    # session[:user_id] = user.id
+    
     
     # @user = user 
     # redirect_to controller: 'restaurant', action: 'index'
