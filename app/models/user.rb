b00bc8573c56c722cac7dd1732ee6c7443eb2cbf
estherkimyunjung <estherkimyunjung@gmail.com>
2020-05-26 09:23:31 -0500

    re-locate validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,5 @@ class User < ApplicationRecord
 
     validates :name, uniqueness: true
     validates :age, numericality: {only_integer: true, greater_than: 14}
-    # validates :user, uniqueness: {scope: :review_id}
     
 end
