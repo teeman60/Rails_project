723efe842bf0ad8f168596e69ee21b8d3951c7c8
estherkimyunjung <estherkimyunjung@gmail.com>
2020-05-26 12:37:55 -0500

    add validates on review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ApplicationRecord
     belongs_to :user
     belongs_to :restaurant
 
-    # validates :user, uniqueness: {scope: :restaurant_id}
-    # validates :date, presence :true
+    validates :user, uniqueness: {scope: :restaurant_id}
+    validates :date, presence :true
+    validates :rating, numericality: {only_integer: true, greater_than: 0, less_than: 10}
+
 end
