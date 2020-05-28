4badcd079eada90fbc4a3402e82ed9bd9abeb556
estherkimyunjung <estherkimyunjung@gmail.com>
2020-05-28 06:07:22 -0500

    add methods in restaurant mode

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,21 @@ class Restaurant < ApplicationRecord
     has_many :foods
     has_one :location
 
+    def avg_rating
+        sum = 0
+        if reviews.length != 0
+            self.reviews.each do |review|
+                sum += review.rating
+            end
+            result = sum/self.reviews.length
+        else
+            result = 0
+        end
+        result
+    end
+
+    def most_recent
+        self.reviews.last(3).map {|rev| rev.comment}
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ApplicationRecord
 
     has_secure_password
 
-    # validates :name, uniqueness: true
-    # validates :age, numericality: {only_integer: true, greater_than: 14}
+    validates :name, uniqueness: true
+    validates :age, numericality: {only_integer: true, greater_than: 14}
+
+    def most_like
+        self.reviews.max{ |a,b| a.rating <=> b.rating }
+    end
     
 end
